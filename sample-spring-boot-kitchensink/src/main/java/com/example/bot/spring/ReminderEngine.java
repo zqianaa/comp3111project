6af2b18d9f1787c01413a333589cf04735ff7448
钱子安 <qwmqza@qianziandeMacBook-Pro.local>
2017-11-01 19:57:14 +0800

    test

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/ReminderEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/ReminderEngine.java
@@ -26,7 +26,7 @@ public class ReminderEngine {
             }
         };
         timer = new Timer();
-        timer.schedule(task, time);
+        timer.schedule(task, 20);
         kc.reminder(time.toString());
     }
     public Date getTime() {
