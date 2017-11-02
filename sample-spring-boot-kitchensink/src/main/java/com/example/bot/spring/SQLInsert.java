4e3d1453af8e558c58bdf9968bc87b3bca587c5d
wing kit Kung <wckung@connect.ust.hk>
2017-11-02 22:25:04 +0800

    't

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLInsert.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLInsert.java
@@ -35,8 +35,9 @@ public class SQLInsert {
 			conn.close(); 
 		} catch (Exception e) { 
 			
-			kc.reminder(e.getMessage()); 
+			
 			kc.reminder("Got an exception! "); 
+			kc.reminder(e.getMessage()); 
 		} 
 
 	}
