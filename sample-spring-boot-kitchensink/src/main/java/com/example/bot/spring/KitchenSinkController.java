2a9886b74db1e460fecbcaa628dc0b6e60cf4d8f
wing kit Kung <wckung@connect.ust.hk>
2017-11-02 17:42:03 +0800

    't

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -323,7 +323,7 @@ public class KitchenSinkController {
 				int height = Integer.parseInt(data[1]);
 				int age = Integer.parseInt(data[2]);
 				reminder(data[3]);
-				SQLInsert re5 =new SQLInsert(USERID,weight,height,age,data[3]);
+				SQLInsert re5 =new SQLInsert(USERID,weight,height,age,data[3], this);
 				replyText(replyToken, "Thanks for using feature 1");
 				break;
 				}
--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLInsert.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLInsert.java
@@ -10,19 +10,19 @@ import java.net.URI;
 @Slf4j
 public class SQLInsert { 
 
-	public SQLInsert (String uSERID ,int w, int h, int a, String data ) { 
+	public SQLInsert (String uSERID ,int w, int h, int a, String data, KitchenSinkController kc ) { 
 		try { 
+			
 			SQLDatabaseEngine engine=new SQLDatabaseEngine() ; 
 			Connection conn =engine.getConnection() ; 
 			Statement st = conn.createStatement();
-			System.out.println("connnection");
 			if(data == "M") {
 				int b =(int) (66.47+ (13.75* w) + (5.0*h) - (6.75* a));
 				st.executeUpdate("INSERT INTO customertable " + 
 						"VALUES ("+ uSERID + "," + w +","+ h +","+ a + ", 'M'," + b +");");
 				st.executeUpdate("INSERT INTO customertable VALUES ('2',2,2,2,'M',2);");
-				System.out.println("test");
-				System.out.println (uSERID+ w+h+a+data);
+				kc.reminder("test");
+				kc.reminder(uSERID+ w+h+a+data);
 			}
 			else if (data== "F") {
 				int b =(int) (66.47+ (13.75* w) + (5.0*h) - (6.75* a));
