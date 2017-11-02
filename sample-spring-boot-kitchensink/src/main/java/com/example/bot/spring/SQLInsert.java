ca5ddbea28b28cd2a1a3dc3816607c2d4b06eb99
wing kit Kung <wckung@connect.ust.hk>
2017-11-02 12:22:33 +0800

    test

---
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLInsert.java
@@ -0,0 +1,35 @@
+package com.example.bot.spring;
+
+import lombok.extern.slf4j.Slf4j;
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.net.URISyntaxException;
+import java.net.URI;
+
+@Slf4j
+public class SQLInsert { 
+
+	public SQLInsert (String name ,int w, int h, int a, String sex ) { 
+		try { 
+			SQLDatabaseEngine engine=new SQLDatabaseEngine() ; 
+			Connection conn =engine.getConnection() ; 
+			Statement st = conn.createStatement();
+			if(sex == "M") {
+				st.executeUpdate("INSERT INTO Customers " + 
+						"VALUES (name,w, , h, a,sex, 66.47+ (13.75 x w) + (5.0 x h) - (6.75 x a))");
+			}
+			else if (sex== "F") {
+				st.executeUpdate("INSERT INTO Customers " + 
+						"VALUES (name,w, , h, a,sex, 66.47+ 665.09 + (9.56 x w) + (1.84 x h) - (4.67 x a)");
+			
+			}
+
+			conn.close(); 
+		} catch (Exception e) { 
+			System.err.println("Got an exception! "); 
+			System.err.println(e.getMessage()); 
+		} 
+
+	}
+}
