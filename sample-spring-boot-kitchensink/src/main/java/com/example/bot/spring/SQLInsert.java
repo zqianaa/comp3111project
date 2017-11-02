97f3c1c77cf7a9945bf09d98887aac9c72c47203
wing kit Kung <wckung@connect.ust.hk>
2017-11-02 14:36:30 +0800

    'tes

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLInsert.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLInsert.java
@@ -19,8 +19,7 @@ public class SQLInsert {
 				int b =(int) (66.47+ (13.75* w) + (5.0*h) - (6.75* a));
 				st.executeUpdate("INSERT INTO customertable " + 
 						"VALUES ("+ uSERID + "," + w +","+ h +","+ a + ", 'M'," + b +");");
-				st.executeUpdate("INSERT INTO customertable " + 
-						"VALUES ('2',2,2,2,'M',"+b+");");
+				st.executeUpdate("INSERT INTO customertable VALUES ('2',2,2,2,'M',2);");
 			}
 			else if (data== "F") {
 				int b =(int) (66.47+ (13.75* w) + (5.0*h) - (6.75* a));
