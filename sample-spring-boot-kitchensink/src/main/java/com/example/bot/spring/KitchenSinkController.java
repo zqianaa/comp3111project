54ed2524e08997187145dee20441e5b1ce5b5100
钱子安 <qwmqza@qianziandeMacBook-Pro.local>
2017-11-03 14:17:46 +0800

    test

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -321,13 +321,12 @@ public class KitchenSinkController {
 
 			}
 			// modified the 'switch' command according to the feature you are implementing.
-			switch(mark2)
-			{
+			switch(mark2) {
 				case 1: {
 					mark2++;
-					this.reminder("test");
+					reminder("test");
 					String[] input = text.split(":");
-					this.reminder(input[0] + input[1]);
+					reminder(input[0] + input[1]);
 					option = new String[input.length/2];
 					price = new int[input.length/2];
 					for (int i = 0; i < input.length; i++) // store menu into array option & price
