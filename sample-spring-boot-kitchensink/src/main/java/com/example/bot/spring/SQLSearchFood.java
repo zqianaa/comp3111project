7800e154b7ccc5a676c4b562814a54780e10db04
钱子安 <qwmqza@qianziandeMacBook-Pro.local>
2017-11-20 17:38:59 +0800

    test

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLSearchFood.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLSearchFood.java
@@ -36,7 +36,9 @@ public class SQLSearchFood implements SQLSearching{
             stmt.setString(1, date+check);
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) {
+                kc.reminder("test1");
                 if (rs.getString(1).substring(0,5).equals(date) && rs.getString(1).substring(5).equals(check)) {
+                    kc.reminder("test2");
                     string = rs.getString(2);
                     string = string + "," + rs.getString(4);
                     string = string + "," + rs.getString(6);
